4172f0010d097909ccade0d3658d997aabd545ab
陳建業 <jianyeh@EEIT>
2017-09-22 12:27:24 +0800

    第4次

--- LabWebServletJdbc/src/misc/EncodingFilter.java
+++ LabWebServletJdbc/src/misc/EncodingFilter.java
@@ -25,6 +25,8 @@ public class EncodingFilter implements Filter {
 		request.setCharacterEncoding("UTF-8");
 		chain.doFilter(request, response);
 		
+		
+		
 	}
 	private FilterConfig filterConfig;
 	@Override
